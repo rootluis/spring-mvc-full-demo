f6661204aef645c8fc29248e3feda05a7b26994a
Luis <rootluis85@gmail.com>
2020-10-16 00:09:25 -0500

    Add all components for the test in GitHub

---
+++ src/mx/com/mejia/spring/dao/AllergyDAO.java
@@ -0,0 +1,11 @@
+package mx.com.mejia.spring.dao;
+
+import java.util.List;
+
+import mx.com.mejia.spring.entity.Allergy;
+
+public interface AllergyDAO {
+	
+	public List<Allergy> getAllergys();
+
+}
